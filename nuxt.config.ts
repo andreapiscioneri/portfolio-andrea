f4f5671686b462b6a890eebd6315474b26efffbd
andrea piscioneri <andrypiscioneri@gmail.com>
2026-05-09 19:31:51 +0200

    test col

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -59,7 +59,7 @@ export default defineNuxtConfig({
     fallback: 'dark',
     classSuffix: '',
     storageKey: 'ap-color-mode',
-    storage: 'cookie',
+    storage: 'localStorage',
   },
 
   i18n: {
